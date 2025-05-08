627843f393b5f9104f26553a4f439d7c3c281780
adnkdev <adnkdeveloper123@gmail.com>
2025-05-08 15:31:22 +1000

    added landing page text and user input functionality

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,34 +1,40 @@
 
 'use client';
 import { useState } from "react";
+import Image from 'next/image';
+
 
 
 export default function LinkedinHomepage() {
 
 
-  const [value, setValue] = useState("");
-
-  function handleChange(e) {
-    setValue(e.target.value);
-  }
+  const [search, setSearch] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
 
   return (
 
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <Image
+            alt="Your Company"
+            src="/images/default-logo.png"
+            width={500}
+            height={500}
+            className="mx-auto h-10 w-auto"
+          />
         <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
           LinkedIn Automation Tool
         </h2>
       </div>
+      <h4 className=" mt-4 text-center text-lg font-bold tracking-tight text-gray-900">
+        Automatically find trending LinkedIn posts, generate fresh content, and publish it to your profile—no writing, no hassle, just results.
+      </h4>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form action="#" method="POST" className="space-y-6">
           <div >
-            
-            <label className="mt-10 text-center text-lg text-gray-500">
-              Search for specific topic
-            </label>
             <div className="mt-2">
               <input
                 id="email"
@@ -37,10 +43,36 @@ export default function LinkedinHomepage() {
                 required
                 autoComplete="email"
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                value={value}
-                onChange={handleChange}
+                value={search}
+                onChange={(e)=>{setSearch(e.target.value)}}
+                placeholder="Search topic"
               />
             </div>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                value={email}
+                onChange={(e)=>{setEmail(e.target.value)}}
+                placeholder="LinkedIn email"
+              />
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                value={password}
+                onChange={(e)=>{setPassword(e.target.value)}}
+                placeholder="LinkedIn password"
+              />
+            </div>
+
           </div>
 
 
@@ -49,7 +81,7 @@ export default function LinkedinHomepage() {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              Submit
             </button>
           </div>
         </form>
